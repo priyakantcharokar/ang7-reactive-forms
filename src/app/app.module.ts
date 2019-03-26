7deb275cfd2382c0d2bb69ddc75828c14948cfea
Priyakant Charokar <priyakantcharokar@MUMAIR002.local>
2019-03-26 08:16:34 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,75 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Add your project credentials
+// Then use it in the imports section below
+const yourFirebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: '<your-database-URL>',
+  projectId: '<your-project-id>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>'
+};
+
+// Delete Me!
+import { firebase } from '../env';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+
+
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { ArrayFormComponent } from './array-form/array-form.component';
+import { ValidFormComponent } from './valid-form/valid-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
+// import { } from '@angular/material/
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    ArrayFormComponent,
+    ValidFormComponent,
+    SubmitFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
+    AngularFireStorageModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatChipsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
